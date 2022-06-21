445372dad13db7c6851d8a9ada21fad2f3f64e2d
sherif <sherif@sherifs-MacBook-Pro.local>
2022-06-21 23:58:28 +0200

    added third view controller

---
+++ playwithGithub/Third Screens/ThirsdViewC.swift
@@ -0,0 +1,21 @@
+//
+//  ThirsdViewC.swift
+//  playwithGithub
+//
+//  Created by sherif on 21/06/2022.
+//
+
+import UIKit
+
+class ThirsdViewC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Sheko"
+    }
+    
+
+  
+
+}
